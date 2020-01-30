6785b48a185dfd30c0a9174f2e8969e463062f3b
roselandroche <r.landroche@gmail.com>
2020-01-29 21:49:52 -0500

    Added a couple routes, started working on migration

--- knexfile.js
+++ knexfile.js
@@ -2,10 +2,10 @@ const sqlite3 = {
   client: 'sqlite3',
   useNullAsDefault: true,
   migrations: {
-    directory: '/data/migrations',
+    directory: './data/migrations',
   },
   seeds: {
-    directory: '/data/seeds',
+    directory: './data/seeds',
   },
 }
 
@@ -14,13 +14,13 @@ module.exports = {
   development: {
     ...sqlite3,
     connection: {
-      filename: '/data/users.db3'
+      filename: './data/users.db3'
     }
   },
   test: {
     ...sqlite3,
     connection: {
-      filename: '/data/usersTest.db3'
+      filename: './data/usersTest.db3'
     }
   }
 };
