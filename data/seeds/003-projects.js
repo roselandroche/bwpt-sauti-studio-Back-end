6b179b9d0a09ad07cd093d34144b69f03fa0f8df
roselandroche <r.landroche@gmail.com>
2020-01-30 20:35:50 -0500

    Writing seeds

--- data/seeds/003-projects.js
+++ data/seeds/003-projects.js
@@ -1,8 +1,8 @@
 
 exports.seed = async (knex) => {
   await knex('projects').insert([
-    { project_name: 'Visage Tome', description: 'Similar to Facebook', project_steps_id: 1 },
-    // { username: 'Ayanda', password: '123', email: 'ayanda@gmail.com', project_id: 1 },
-    // { username: 'Mhambi', password: 'abc123', email: 'mhambi@gmail.com', project_id: 1 }
+    { project_name: 'Build Wells', description: 'Locate places to build wells, fundraise' },
+    { project_name: 'Educate', description: 'Connect to world, educate on current African issues' },
+    { project_name: 'Transport', description: 'Give options for safest way to travel between locations' }
   ])
 };
--- data/seeds/004-project_steps.js
+++ data/seeds/004-project_steps.js
@@ -1,13 +1,8 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = async (knex) => {
+  await knex('projects').insert([
+    { step_name: 'Create a team', description: 'Connect with engineers, scientists', project_id: 1 },
+    { step_name: 'Create a team', description: 'Find journalists, web developer', project_id: 2 },
+    { step_name: 'Create a team', description: 'Find locals who travel often, interview them', project_id: 3 },
+  ])
 };
