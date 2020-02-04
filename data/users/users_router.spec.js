a16ba01641d8723d6b978cfaff2a96e48ff22c09
roselandroche <r.landroche@gmail.com>
2020-02-03 20:36:47 -0500

    Coverage

--- data/users/users_router.spec.js
+++ data/users/users_router.spec.js
@@ -1,14 +1,24 @@
 const supertest = require('supertest')
 const server = require('../../index')
+const db = require('../dbConfig')
+
+beforeAll(async () => {
+    console.log(`called`)
+    await db.truncate('users')
+})
 
 describe('users router', () => {
     describe('register', () => {
         it('should register a new user, return 201', async () => {
-            const response = await supertest(server).post('/register').send({ 
+            const res = await supertest(server).post('/register').send({ 
                 username: 'Don',
                 password: '123',
                 email: 'don@gmail.com'
              })
+             expect(res.status).toBe(201)
+             expect(res.type).toBe('application/json')
+             expect(res.body.username).toBe('Don')
         })
     })
+    
 })
