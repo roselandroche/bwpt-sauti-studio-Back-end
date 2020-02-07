856fc2a41220bdf97712f369f753493b1d9b9d4b
roselandroche <r.landroche@gmail.com>
2020-02-06 20:03:55 -0500

    Change iteration

--- data/users/users_model.js
+++ data/users/users_model.js
@@ -8,7 +8,7 @@ function find() {
 }
 // add
 async function add(user) {
-    user.password = await bcrypt.hash(user.password, 13)
+    user.password = await bcrypt.hash(user.password, 10)
 
     const [id] = await db('users')
         .insert(user)
